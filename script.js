ca93b346c30959cde929dae89e7264c36558166d
Nyarlathotep <eduardasales273@gmail.com>
2025-08-08 15:32:34 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+window.revelar = ScrollReveal({reset: false})
+
+revelar.reveal('.efeito-home', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+    
+})
+
+revelar.reveal('.efeito-about', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-education', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-experience', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-projects', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-production', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-contact', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
 document.addEventListener('DOMContentLoaded', () => {
   // Fecha todos os detalhes dentro de uma seção
   function collapseSection(section) {
